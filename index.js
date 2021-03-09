cf5717f90340effb4e314b3a8fa1db9957338222
Jolldev <69489813+Jolldev@users.noreply.github.com>
2021-03-08 21:00:44 -0600

    Add files via upload
    Updated main page with client provided text. Redid first text box underneath carousel adding floating photo on medium and up. Added buttons to bottom of alternate pages to navigate.

--- index.js
+++ index.js
@@ -1,49 +1,4 @@
 
-// Makes all testimonial text boxes on main page same height by matching heighest
-// function carouselNormalization() {
-//     var items = $('.carousel-img'), //grab all slides
-//         heights = [], //create empty array to store height values
-//         tallest; //create variable to make note of the tallest slide
-
-//     if (items.length) {
-//         function normalizeHeights() {
-//             items.each(function () { //add heights to array
-//                 heights.push($(this).height());
-//             });
-//             tallest = Math.max.apply(null, heights); //cache largest value
-//             items.each(function () {
-//                 $('.carousel-img').css('height', tallest + 'px');
-//             });
-//         };
-//         normalizeHeights();
-
-//         $(window).on('orientationchange', function () {
-//             $('.carousel').carousel(0); //resets carousel to first (tallest) slide
-//             tallest = 0, heights.length = 0; //reset vars
-//             items.each(function () {
-//                 $(this).css({ height: '0' });
-//                 $(this).css('min-height', '0'); //reset min-height
-//                 $(this).css({ height: 'auto' });
-//             });
-//             carouselNormalization(); //run it again 
-//         });
-
-//         $(window).on('resize', function () {
-//             tallest = 0, heights.length = 0; //reset vars
-//             $('.carousel').carousel(0); //resets carousel to first (tallest) slide
-//             items.each(function () {
-//                 $(this).css({ height: '0' });
-//                 $(this).css('min-height', '0'); //reset min-height
-//                 $(this).css({ height: 'auto' });
-//             });
-//             carouselNormalization(); //run it again 
-//         });
-//     }
-// }
-
-
-
-
 
 /**
  * Wait until all the assets have been loaded so a maximum height 
@@ -91,9 +46,9 @@ function windowSize() {
     }
 }
 
-window.onload = function () {
-    windowSize();
-}
+// window.onload = function () {
+//     windowSize();
+// }
 window.addEventListener('resize', windowSize);
 window.addEventListener("orientationchange", windowSize);
 document.addEventListener("DOMContentLoaded", windowSize);
